2e194b4f0e85960cca69ef87a2ac4e4a64c897c4
Simon Wächter <waechter.simon@gmail.com>
2017-01-17 21:14:33 +0100

    Add more fprog code

--- Module/fprog/src/week13/Trees.hs
+++ Module/fprog/src/week13/Trees.hs
@@ -12,7 +12,13 @@ sumT (Leaf a) = a
 sumT (Node (xs)) = sum [sumT x | x <- xs]
 
 flattenT :: Tree a -> [a]
+flattenT (Leaf a) = [a]
+flattenT (Node xs) = foldr (\x accu -> accu ++ (flattenT x)) [] xs
 
-occursT :: Eq a => a -> Tree a -> Bool
+-- occursT :: Eq a => a -> Tree a -> Bool
+-- occursT e (Leaf a) = e == a
+-- occursT e (Node xs) = foldr(\x accu -> if occursT e x then True else accu) False xs
 
-lookupT :: Eq a => a -> Tree (a, b) -> Maybe b
+-- lookupT :: Eq a => a -> Tree (a, b) -> Maybe b
+-- lookupT e (Leaf a) = if e == a then Just a else Nothing
+-- lookupT e (Node xs) = foldr(\x accu -> if lookupT e x then Just e else accu) Nothing xs
