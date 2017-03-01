435c06c9696faf7b70bd98e1f4cbc2dd5c9f936f
Simon Wächter <waechter.simon@gmail.com>
2017-03-01 19:58:52 +0100

    Add the weekly syspr exercises

---
+++ Module/syspr/workspace/week2/exercise2/main.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct address {
+    char name[50];
+    char street[100];
+    short number;
+    long zip;
+    char town[50];
+};
+
+typedef struct address address_t;
+
+typedef address_t *address_ptr_t;
+
+typedef char *string_t;
+
+typedef short number_t;
+
+typedef long zip_t;
+
+typedef void *params_t;
+
+void copy_address_by_values(address_ptr_t address, string_t name, string_t street, number_t number, zip_t zip, string_t town) {
+    strcpy(address->name, name);
+    strcpy(address->street, street);
+    strcpy(address->town, town);
+    address->number = number;
+    address->zip = zip;
+}
+
+/*void copy_address_by_array(address_ptr_t address, params_t params) {
+    copy_address_by_values(address, (string_t) &params[0], (string_t) &params[1], (number_t) &params[2], (zip_t) &params[3], (string_t) &params[4]);
+}*/
+
+
+address_ptr_t copy_address_by_address(address_ptr_t addressout, address_ptr_t addressin) {
+    copy_address_by_values(addressout, addressin->name, addressin->street, addressin->number, addressin->zip, addressin->town);
+    return addressout;
+}
+
+address_ptr_t create_address(string_t name, string_t street, number_t number, zip_t zip, string_t town) {
+    address_ptr_t address = (address_t *) malloc(sizeof(address_t));
+    copy_address_by_values(address, name, street, number, zip, town);
+    return address;
+}
+
+address_ptr_t delete_address(address_ptr_t address) {
+    free(address);
+    return NULL;
+}
+
+void display_address(address_ptr_t address) {
+    printf("===== Address =====\n");
+    printf("Name %s\n", address->name);
+    printf("Street %s\n", address->street);
+    printf("Number %d\n", address->number);
+    printf("Zip %d\n", address->zip);
+    printf("Town %s\n", address->town);
+}
+
+int main(void) {
+    // Define the address values
+    string_t name = "Simon Wächter";
+    string_t street = "Somewhere Street" + '\0';
+    string_t town = "Basel";
+    number_t number = 42;
+    zip_t zip = 4053;
+
+    // Create an address
+    address_ptr_t address1 = create_address(name, street, number, zip, town);
+
+    // Copy the address
+    address_ptr_t address2 = create_address("Name", "Street", 100, 5, "Town");
+    copy_address_by_values(address2, name, street, number, zip, town);
+
+    // Copy the address into the other address
+    address_ptr_t address3 = copy_address_by_address(address2, address1);
+    printf("The pointer address of the copied address is %p to %p is %p\n", address1, address2, address3);
+
+    // Copy the parameters into the other address
+    /*params_t params[] = {&name, &street, &number, &zip, &town};
+    copy_address_by_array(address2, params);*/
+
+    // Delete the addresses
+    address1 = delete_address(address1);
+    address2 = delete_address(address2);
+    printf("The pointer addresses of the deleted addresses are %p and %p\n", address1, address2);
+
+    // Create the addresses
+    int studentnumber = 10;
+    address_ptr_t addresses[studentnumber];
+    for (int i = 0; i < studentnumber; i++) {
+        char name[] = "Name ";
+        char realname[50] = {};
+        sprintf(realname, "%s%d", name, i);
+        addresses[i] = create_address(realname, "Street", 100, 4000, "Town");
+    }
+
+    // Display the addresses
+    for (int i = 0; i < studentnumber; i++) {
+        display_address(addresses[i]);
+    }
+
+    // Delete the addresses
+    for (int i = 0; i < studentnumber; i++) {
+        addresses[i] = delete_address(addresses[i]);
+    }
+
+    return 0;
+}
