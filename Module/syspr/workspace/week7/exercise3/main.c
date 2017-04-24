e841172989bf9b92dcc29bf8c7943e3f63c54158
Simon Wächter <waechter.simon@gmail.com>
2017-04-24 21:27:28 +0200

    Fix the example

--- Module/syspr/workspace/week7/exercise3/main.c
+++ Module/syspr/workspace/week7/exercise3/main.c
@@ -2,39 +2,48 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+struct command {
+    const char **argv;
+};
+
+int spawn_process(int in, int out, struct command *singlecommand) {
+    pid_t pid;
+    if ((pid = fork()) == 0) {
+        if (in != 0) {
+            dup2(in, 0);
+            close(in);
+        }
+        if (out != 1) {
+            dup2(out, 1);
+            close(out);
+        }
+        return execvp(singlecommand->argv[0], (char *const *) singlecommand->argv);
+    }
+    return pid;
+}
+
+int fork_pipes(int n, struct command *commands) {
+    int i, in = 0;
+    int fd[2];
+
+    for (i = 0; i < n - 1; ++i) {
+        pipe(fd);
+        spawn_process(in, fd[1], commands + i);
+        close(fd[1]);
+        in = fd[0];
+    }
+
+    if (in != 0) {
+        dup2(in, 0);
+    }
+
+    return execvp(commands[i].argv[0], (char *const *) commands[i].argv);
+}
+
 int main(void) {
-    int p1[2];
-    int p2[2];
-
-    if (pipe(p1)) {
-        perror("Unable to create the first pipe");
-        return EXIT_FAILURE;
-    }
-
-    int pid;
-
-    pid = fork();
-    if(pid == 0) {
-        dup2(p1[1], STDOUT_FILENO);
-        close(p1[0]);
-        execlp("/bin/cat", "/bin/cat", "/etc/passwd");//, "|", "grep", "root");
-    } else {
-        dup2(p1[0], STDIN_FILENO);
-        close(p1[1]);
-
-        if(pipe(p2)) {
-            perror("Unable to create the first pipe");
-            return EXIT_FAILURE;
-        }
-
-        pid = fork();
-        if(pid == 0) {
-            dup2(p2[1], STDOUT_FILENO);
-            close(p2[0]);
-            execl("/bin/grep", "/bin/grep");
-        } else {
-
-        }
-    }
-    return EXIT_SUCCESS;
+    const char *cat[] = {"cat", "/etc/passwd", 0};
+    const char *grep[] = {"grep", "root", 0};
+    struct command commands[] = {{cat},
+                                 {grep}};
+    return fork_pipes(2, commands);
 }
